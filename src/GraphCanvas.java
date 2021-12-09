d347cc4e06c6cad97fc39d5a58ea8bf0370e07e9
Orel Zamler <orelzx13@gmail.com>
2021-12-09 22:51:08 +0200

    GUI progress

--- src/GraphCanvas.java
+++ src/GraphCanvas.java
@@ -2,6 +2,7 @@ import api.DirectedWeightedGraph;
 import api.EdgeData;
 import api.GeoLocation;
 import api.NodeData;
+import org.w3c.dom.css.RGBColor;
 
 import java.awt.*;
 import java.awt.geom.Line2D;
@@ -18,17 +19,23 @@ public class GraphCanvas extends JPanel {
     public DirectedWeightedGraph graph;
     final int width = 900;
     final int height = 750;
-    private Dimension size = new Dimension(width, height);
     public final Dimension PreferredButtonSize = new Dimension(50, 25);
+    private Dimension size = new Dimension(width, height);
     public double NodeMinX = Double.MAX_VALUE;
     public double NodeMinY = Double.MAX_VALUE;
     public double NodeMaxX = Double.MIN_VALUE;
     public double NodeMaxY = Double.MIN_VALUE;
 
+    final int BLACK = 0;
+    final int BLUE = 0;
+    final int ORANGE = 0;
+
+    String fileName = "data/G3.json";
+
     /** Constructor */
     public GraphCanvas() {
         graphAlgo = new MyDWGAlgorithm();
-        graphAlgo.load("data/G1.json");
+        graphAlgo.load(fileName);
         graph = graphAlgo.getGraph();
         getMinMaxValues();
     }
@@ -36,7 +43,9 @@ public class GraphCanvas extends JPanel {
     public GraphCanvas(String file) {
         graphAlgo = new MyDWGAlgorithm();
         graphAlgo.load(file);
+        fileName = file;
         graph = graphAlgo.getGraph();
+        getMinMaxValues();
     }
 
     public void getMinMaxValues() {
@@ -68,10 +77,10 @@ public class GraphCanvas extends JPanel {
     public void paintComponent(Graphics g) {
         // paint edges
         Graphics2D g2d = (Graphics2D) g;
-        double deltaX = NodeMaxX - NodeMinX;
-        double deltaY = NodeMaxY - NodeMinY;
-        double xScaled = (width / deltaX)*0.75;
-        double yScaled = (width / deltaY) *0.75;
+        double dx = NodeMaxX - NodeMinX;
+        double dy = NodeMaxY - NodeMinY;
+        double xScaled = (width / dx)*0.75;
+        double yScaled = (width / dy) *0.75;
 
         g2d.setPaint(Color.BLACK);
         Iterator<EdgeData> edgeIter = graph.edgeIter();
@@ -83,7 +92,6 @@ public class GraphCanvas extends JPanel {
             double y1 = (int)((geo1.y() - NodeMinY) * yScaled*0.97 + 30);
             double x2 = (int)(((geo2).x() - NodeMinX) * xScaled*0.97 + 30);
             double y2 = (int)((geo2.y() - NodeMinY) * yScaled*0.97 + 30);
-            g2d.setColor(Color.BLACK);
             g2d.setStroke(new BasicStroke(1));
             g2d.draw(new Line2D.Double(x1+15, y1+15, x2+15, y2+15));
             drawArrowHead(g2d, Math.atan2(y2 - y1+15, x2 - x1+15), x2+15, y2+15);
@@ -95,58 +103,19 @@ public class GraphCanvas extends JPanel {
             // Linearly map the point
             double x = (node.getLocation().x() - NodeMinX)*xScaled*0.97+30;
             double y = (node.getLocation().y() - NodeMinY)*yScaled*0.97+30;
-            g.setColor(Color.BLUE);
+            if (node.getTag() == 0){
+                g.setColor(Color.BLACK);
+            }else if(node.getTag()==1){
+                g.setColor(Color.BLUE);
+            }else{
+                g.setColor(Color.ORANGE);
+            }
             g.drawOval((int) x, (int) y, 30, 30);
             Font f = new Font("ariel", Font.BOLD, 16);
             g.setFont(f);
             g.setColor(Color.BLACK);
             g.drawString(node.getKey() + "", (int) x, (int) y-5);
         }
-    }
-
-    /**
-     * Draw an arrow line between two points, revised from the code of @phibao37
-     * http://stackoverflow.com/questions/2027613/how-to-draw-a-directed-arrow-line-in-java
-     *
-     * @param x1 x-position of first point
-     * @param y1 y-position of first point
-     * @param x2 x-position of second point
-     * @param y2 y-position of second point
-     * @param d  the width of the arrow
-     * @param h  the height of the arrow
-     * @return the distance between two points
-     */
-    private double paintArrowLine(Graphics2D g, int x1, int y1, int x2, int y2, int d, int h, Double dist) {
-        int dx = x2 - x1, dy = y2 - y1;
-        double D = Math.sqrt(dx * dx + dy * dy);
-        double sin = dy / D, cos = dx / D;
-
-        //change the end point of the line, r = 40
-        x2 = (int) (x2 - 10 * cos);
-        y2 = (int) (y2 - 10 * sin);
-        D = D - 10; // change the length of the line
-        double xm = D - d, xn = xm, ym = h, yn = -h, x;
-
-        x = xm * cos - ym * sin + x1;
-        ym = xm * sin + ym * cos + y1;
-        xm = x;
-
-        x = xn * cos - yn * sin + x1;
-        yn = xn * sin + yn * cos + y1;
-        xn = x;
-
-        int[] xpoints = {x2, (int) xm, (int) xn};
-        int[] ypoints = {y2, (int) ym, (int) yn};
-
-        g.drawLine(x1, y1, x2, y2);
-        g.fillPolygon(xpoints, ypoints, 3);
-        // paint the distance
-        if (dist == -1.0) {
-            // the actual distance(D+40) between two points next to the tail (D=the length of the arrow)
-            dist = Math.round((D + 10) * 100) / 100.00;
-        }
-        g.drawString(Double.toString(dist), (int) (xm - 30 * cos), (int) (ym - 30 * sin));
-        return dist;
     }
 
     // taken from https://coderanch.com/t/339505/java/drawing-arrows
@@ -163,9 +132,7 @@ public class GraphCanvas extends JPanel {
         g2.draw(new Line2D.Double(x0, y0, x, y));
     }
 
-
-    /*Repaint every thing to the default color*/
-    public void refresh() {
+    public void refresh(){
         repaint();
     }
 
@@ -181,4 +148,49 @@ public class GraphCanvas extends JPanel {
     public Dimension getSize() {
         return size;
     }
+    /**
+     * Paint the traversal path to red
+     * @param path the list of edges in the traversal path
+     * @return whether there is a traversal to paint or not
+     */
+    public Boolean paintTraversal(LinkedList<EdgeData> path){
+        boolean painting;
+        if (path.isEmpty()){
+            painting = false;
+            return painting;
+        } else {
+            painting = true;
+        }
+        // set every thing to gray, and set the start point to orange
+        Iterator<EdgeData> edgeIter =graph.edgeIter();
+        while (edgeIter.hasNext()){
+            edgeIter.next().setTag(Color.black.getRGB());
+        }
+        Iterator<NodeData> nodeIter = graph.nodeIter();
+        while (nodeIter.hasNext()){
+            nodeIter.next().setTag(Color.lightGray.getRGB());
+        }
+        path.get(0).setTag(Color.ORANGE.getRGB());
+        repaint();
+
+        for (EdgeData edge:path){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignore) {
+            }
+            // Color the current edge
+            edge.setTag(BLUE);
+            graph.getNode(edge.getDest()).setTag(ORANGE);
+            repaint();
+            try {
+                Thread.sleep(800);
+            } catch (InterruptedException ignore) {
+            }
+            edge.setTag(BLACK);
+            graph.getNode(edge.getDest()).setTag(0);
+            repaint();
+        }
+        return painting;
+    }
+
 }
